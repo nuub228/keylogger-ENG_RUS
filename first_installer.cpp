893a380a718212efb9afa28b24008e5b1ecdd883
nuub228 <54170775+nuub228@users.noreply.github.com>
2021-04-30 14:27:53 +0300

    Add files via upload

---
+++ first_installer.cpp
@@ -0,0 +1,13 @@
+﻿#include <windows.h>
+#include <Lmcons.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	char username[UNLEN + 1];
+	DWORD username_len = UNLEN + 1;
+	GetUserNameA(username, &username_len);
+	cout << username;
+}
